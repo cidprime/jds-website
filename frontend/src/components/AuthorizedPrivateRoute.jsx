76cd81f4f1dbdd6ac544013d83f1288a37fc6b6a
A.Sidiki Ouedraogo <osidiki226@gmail.com>
2025-03-31 23:57:53 +0000

    Update AuthorizedPrivateRoute to use Redux for user state management

--- frontend/src/components/AuthorizedPrivateRoute.jsx
+++ frontend/src/components/AuthorizedPrivateRoute.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { useSelector } from "react-redux"
+import { Navigate, Outlet } from "react-router-dom";
 
 export default function AuthorizedPrivateRoute() {
   const { currentUser } = useSelector((state) => state.user);
